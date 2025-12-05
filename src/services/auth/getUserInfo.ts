868a14e0197b5689ad5508f637970d88f0d786b6
emonpappu17 <emon.pappu.5@gmail.com>
2025-12-05 23:27:56 +0600

    add: get user info server aciotn

---
+++ src/services/auth/getUserInfo.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { serverFetch } from "@/lib/server-fetch";
+import { IUser } from "@/types/user.interface";
+import { getCookie } from "./tokenHandlers";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+
+export const getUserInfo = async (): Promise<IUser | any> => {
+    let userInfo: IUser | any;
+    try {
+
+        const response = await serverFetch.get("/user/me", {
+            cache: "force-cache",
+            next: { tags: ["user-info"] }
+        })
+
+        const result = await response.json();
+
+        if (result.success) {
+            const accessToken = await getCookie("accessToken");
+
+            if (!accessToken) {
+                throw new Error("No access token found");
+            }
+
+            const verifiedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string) as JwtPayload;
+
+            userInfo = {
+                name: verifiedToken.name || "Unknown User",
+                email: verifiedToken.email,
+                role: verifiedToken.role,
+            }
+        }
+
+        userInfo = {
+            name: result.data.fullName || "Unknown User",
+            ...result.data
+        };
+
+        return userInfo;
+    } catch (error: any) {
+        console.log(error);
+        return {
+            id: "",
+            name: "Unknown User",
+            email: "",
+            role: "USER",
+        };
+    }
+}
--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,1 +1,38 @@
 export type UserRole = "ADMIN" | "USER";
+export enum Gender {
+    MALE = "MALE",
+    FEMALE = "FEMALE",
+    // OTHER = "OTHER",
+}
+
+export interface IUser {
+    id: string;
+    email: string;
+    password: string;
+    role: UserRole;                // enum type you defined in Prisma
+    isActive: boolean;
+    isDeleted: boolean;
+
+    // Profile
+    fullName: string;
+    profileImage?: string;
+    bio?: string;
+    phone?: string;
+    currentLocation?: string;
+    gender?: Gender;           // enum type you defined in Prisma
+    isVerified: boolean;
+    interests: string[];
+    visitedCountries: string[];
+
+    createdAt: Date;
+    updatedAt: Date;
+
+    // Relations
+    // payments: Payment[];
+    // sentRequests: TravelRequest[];
+    // receivedRequests: TravelRequest[];
+    // travelPlans: TravelPlan[];
+    // reviewsGiven: Review[];
+    // reviewsReceived: Review[];
+    // subscriptions: Subscription[];
+}
